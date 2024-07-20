9e39d34ec298c2d5dd9f2dfaec417fedadad9833
Federico <fed_v@hotmail.com>
2024-07-19 23:55:41 -0400

    Finished transitions

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,16 +3,33 @@ import React from 'react';
 // import images
 import WomanImg from '../img/contact/woman.png';
 
+// import motion
+import { motion } from 'framer-motion';
+
+// import transition
+import { transition1 } from '../transitions';
+
 const Contact = () => {
-  return <section className='section'>
+  return <motion.section 
+    initial={{ opacity: 0, y: '100%' }} 
+    animate={{ opacity: 1, y: 0 }} 
+    exit={{ opacity: 0, y: '100%' }}
+    transition={transition1}
+    className='section'
+  >
     <div className='container mx-auto h-full'>
 
       <div className='flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left'>
 
         {/* bg */}
-        <div className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 right-0 top-72 z-10'>
-
-        </div>
+        <motion.div 
+          initial={{ opacity: 0, y: '100%' }} 
+          animate={{ opacity: 1, y: 0 }} 
+          exit={{ opacity: 0, y: '100%' }}
+          transition={transition1}
+          className='hidden lg:flex bg-[#eef7f9] absolute bottom-0 right-0 top-72 z-10'
+        >
+        </motion.div>
       
         {/* text & form*/}
         <div className='lg:flex-1 lg:pt-32 px-4'>
@@ -31,14 +48,19 @@ const Contact = () => {
         </div>
 
         {/* image */}
-        <div className='lg:flex-1'>
+        <motion.div 
+          initial={{ opacity: 0, y: '100%' }} 
+          animate={{ opacity: 1, y: 0 }} 
+          exit={{ opacity: 0, y: '100%' }}
+          transition={{ transition: transition1, duration: 1.5 }}
+          className='lg:flex-1'>
           <img src={WomanImg} alt='woman' title='woman' />
-        </div>
+        </motion.div>
 
       </div>
 
     </div>
-  </section>;
+  </motion.section>;
 };
 
 export default Contact;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -16,12 +16,24 @@ import { motion } from 'framer-motion';
 import { transition1 } from '../transitions';
 
 const Portfolio = () => {
-  return <section className='section'>
+  return <motion.section 
+    initial={{ opacity: 0, y: '100%' }} 
+    animate={{ opacity: 1, y: 0 }} 
+    exit={{ opacity: 0, y: '100%' }}
+    transition={transition1}
+    className='section'
+  >
     <div className='container mx-auto h-full relative'>
         <div className='flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8'>
 
             {/* text */}
-            <div className='flex flex-col lg:items-start'>
+            <motion.div 
+              initial={{ opacity: 0, y: '-80%' }} 
+              animate={{ opacity: 1, y: 0 }} 
+              exit={{ opacity: 0, y: '-80%' }}
+              transition={transition1}
+              className='flex flex-col lg:items-start'
+            >
                 <h1 className='h1'>Portfolio</h1>
                 <p className='mb-12 max-w-sm'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -30,7 +42,7 @@ const Portfolio = () => {
                   Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure 
                 </p>                
                 <Link to='/contact' className='btn mb-[30px] mx-auto lg:mx-0'>Hire me</Link>
-            </div>
+            </motion.div>
 
             {/* image grid */}
             <div className='grid grid-cols-2 lg:gap-2'>
@@ -54,7 +66,7 @@ const Portfolio = () => {
         </div>
 
     </div>
-  </section>;
+  </motion.section>;
 };
 
 export default Portfolio;
