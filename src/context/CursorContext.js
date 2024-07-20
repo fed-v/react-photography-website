a3c150da8021ea8baafeaa29da0f83c5e793180d
Federico <fed_v@hotmail.com>
2024-07-20 10:47:12 -0400

    Add cursor context provider

--- src/context/CursorContext.js
+++ src/context/CursorContext.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import { useState, useEffect, createContext } from 'react';
 
-const CursorContext = () => {
-  return <div>CursorContext</div>;
+// create context
+export const CursorContext = createContext();
+
+// Create context provider
+const CursorProvider = ({ children }) => {
+  // Cursor pointer state
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const move = (e) => {
+      setCursorPos({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener('mousemove', move);
+
+    // remove event
+    return () => {
+      window.removeEventListener('mousemove', move);
+    };
+  }, []); // Added an empty dependency array to ensure useEffect runs once
+
+  return (
+    <CursorContext.Provider value={cursorPos}>
+      {children}
+    </CursorContext.Provider>
+  );
 };
 
-export default CursorContext;
+export default CursorProvider;
