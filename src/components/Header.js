f65c70a1cd6d5fbd747ae1c2f98eccdddf175402
Federico <fed_v@hotmail.com>
2024-07-20 11:57:19 -0400

    Added cursor to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // import components
 import Socials from './Socials';
@@ -8,17 +8,31 @@ import Logo from '../img/header/logo.svg';
 // import link
 import { Link } from 'react-router-dom';
 
+// import cursor context
+import { CursorContext } from '../context/CursorContext';
+
 const Header = () => {
+
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
   return <header className='fixed w-full px-[30px] lg-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
     <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
 
       {/* logo */}
-      <Link to='/' className='max-w-[200px]'>
+      <Link 
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler} 
+        to='/' className='max-w-[200px]'
+      >
         <img src={Logo} alt='logo' title='logo' />
       </Link>
         
       {/* nav - initially hidden - show on desktop mode */}
-      <nav className='hidden lg:flex gap-x-4'>
+      <nav 
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler} 
+        className='hidden lg:flex gap-x-4'
+      >
         <Link to='/' className='text-[#696c6d] hover:text-primary transition'>Home</Link>
         <Link to='/about' className='text-[#696c6d] hover:text-primary transition'>About</Link>
         <Link to='/contact' className='text-[#696c6d] hover:text-primary transition'>Contact</Link>
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,39 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // import icons
-
 import { ImFacebook, ImTwitter, ImPinterest, ImInstagram, ImYoutube } from 'react-icons/im';
 
+// import cursor context
+import { CursorContext } from '../context/CursorContext';
+
 const Socials = () => {
-  return <div className='hidden xl:flex ml-24'>
-    <ul className='flex gap-x-4'>
-      <li>
-        <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
-          <ImFacebook />
-        </a>
-      </li>
-      <li>
-        <a href='https://www.twitter.com/' target='_blank' rel='noreferrer'>
-          <ImTwitter />
-        </a>
-      </li>
-      <li>
-        <a href='https://www.pinterest.com/' target='_blank' rel='noreferrer'>
-          <ImPinterest />
-        </a>
-      </li>
-      <li>
-        <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
-          <ImInstagram />
-        </a>
-      </li>
-      <li>
-        <a href='https://www.youtube.com/' target='_blank' rel='noreferrer'>
-          <ImYoutube />
-        </a>
-      </li>
-    </ul>
-  </div>;
+
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
+  return (
+    <div 
+    onMouseEnter={mouseEnterHandler}
+    onMouseLeave={mouseLeaveHandler}  
+    className='hidden xl:flex ml-24'
+    >
+      <ul className='flex gap-x-4'>
+        <li>
+          <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
+            <ImFacebook />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.twitter.com/' target='_blank' rel='noreferrer'>
+            <ImTwitter />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.pinterest.com/' target='_blank' rel='noreferrer'>
+            <ImPinterest />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
+            <ImInstagram />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.youtube.com/' target='_blank' rel='noreferrer'>
+            <ImYoutube />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
