127eaae06730bceefd9568acf6a3fb936cb1b8cb
Federico <fed_v@hotmail.com>
2024-08-03 22:39:14 -0400

    Removed Tailwind CSS from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,36 @@ import MobileNav from './MobileNav';
 import Logo from '../assets/images/header/logo.svg';
 import { Link } from 'react-router-dom';
 import { CursorContext } from '../context/CursorContext';
+import '../assets/styles/Header.css';
 
 const Header = () => {
-
+  
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
 
   return (
-    <header className='fixed w-full px-[30px] lg-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
-      <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
-
+    <header className='header'>
+      <div className='header-container'>
+        
         {/* logo */}
         <Link 
           onMouseEnter={mouseEnterHandler}
           onMouseLeave={mouseLeaveHandler} 
-          to='/' className='max-w-[200px]'
+          to='/' 
+          className='logo'
         >
           <img src={Logo} alt='logo' title='logo' />
         </Link>
-          
+        
         {/* nav - initially hidden - show on desktop mode */}
         <nav 
           onMouseEnter={mouseEnterHandler}
           onMouseLeave={mouseLeaveHandler} 
-          className='hidden lg:flex gap-x-4'
+          className='nav'
         >
-          <Link to='/' className='text-[#696c6d] hover:text-primary transition'>Home</Link>
-          <Link to='/about' className='text-[#696c6d] hover:text-primary transition'>About</Link>
-          <Link to='/contact' className='text-[#696c6d] hover:text-primary transition'>Contact</Link>
-          <Link to='/portfolio' className='text-[#696c6d] hover:text-primary transition'>Portfolio</Link>
+          <Link to='/' className='nav-link'>Home</Link>
+          <Link to='/about' className='nav-link'>About</Link>
+          <Link to='/contact' className='nav-link'>Contact</Link>
+          <Link to='/portfolio' className='nav-link'>Portfolio</Link>
         </nav>
         
       </div>
